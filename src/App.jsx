b5197d98f50da2d2882ddecbce117d4677727115
Jasmin Hedlund <jasminhedlund@Jasmins-MacBook-Air.local>
2025-04-21 08:06:41 +0200

    foldername

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 import "./index.css"
-import Hero from "./static sections/Hero/Hero.jsx"
-import Skills from "./static sections/Skills/Skills.jsx"
+import Hero from "./static-sections/Hero/Hero.jsx"
+import Skills from "./static-sections/Skills/Skills.jsx"
 import Project from "./sections/Projects/Projects.jsx"
-import Tech from "./static sections/Tech/Tech.jsx"
+import Tech from "./static-sections/Tech/Tech.jsx"
 import Journey from "./sections/Journey/Journey.jsx"
-import Footer from "./static sections/Footer/Footer.jsx"
+import Footer from "./static-sections/Footer/Footer.jsx"
 
 
 const App = () => {
