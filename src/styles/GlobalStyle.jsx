c8dca953f477ffdcf6bc125daca3856537af5dfc
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-05-08 12:25:27 +0200

    max-width main on desktop> +font-size on all H changed + boxshadow added project + more space on cards

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -11,16 +11,16 @@ const GlobalStyle = createGlobalStyle`
   }
 
   *, *::before, *::after {
-   box-sizing: border-box;
-}
+    box-sizing: border-box;
+  }
 
-main {
-  max-width: 100vw;         
-  overflow-x: hidden;       
-  padding-inline: 16px;    
-  display: flex;            
-  flex-direction: column;   
-}
+  main {
+    max-width: 100vw;         
+    overflow-x: hidden;       
+    padding-inline: 16px;    
+    display: flex;            
+    flex-direction: column;   
+  }
 
   body {
     margin: 0;
@@ -37,53 +37,49 @@ main {
   }
 
   h1 {
-  font-family: var(--font-heading);
-  font-size: 32px;
+    font-family: var(--font-heading);
+    font-size: 28px;
   }
 
   h2 {
-  font-family: var(--font-heading);
-  font-size: 24px;
-  text-align:center;
-  margin-bottom: 30px;
-}
-  
+    font-family: var(--font-heading);
+    font-size: 22px;
+    text-align: center;
+    margin-bottom: 30px;
+  }
 
   h3 {
-  font-family: var(--font-heading);
-  font-size: 18px;
+    font-family: var(--font-heading);
+    font-size: 18px;
   }
 
   h4 {
-  font-family: var(--font-heading);
-  font-size: 16px;
-
+    font-family: var(--font-heading);
+    font-size: 16px;
   }
 
-a {
-  color: var(--color-text);
-  text-decoration: none;
+  a {
+    color: var(--color-text);
+    text-decoration: none;
 
-&:hover { 
-  text-decoration: underline;
-  color: var(--color-text);
-}
+    &:hover { 
+      text-decoration: underline;
+      color: var(--color-text);
+    }
+  }
 
-
-
-@media (min-width: 668px) {
+  @media (min-width: 668px) {
     h1 {
       font-size: 48px;
     }
 
     h2 {
-      font-size: 32px;
+      font-size: 36px;
     }
 
     h3 {
-    font-size: 20px;
-  }
-
+      font-size: 20px;
+    }
   }
 
   @media (min-width: 1024px) and (max-width: 1600px) {
@@ -92,23 +88,20 @@ a {
     }
 
     h2 {
-      font-size: 65px;
+      font-size: 64px;
     }
 
     h3 {
-    font-size: 22px;
-  }
+      font-size: 24px;
+    }
 
-  main {
-    max-width: 1280px;
-    margin: 0 auto;
-    padding-inline: 24px;
-  }
-}
-
-
-   
+    main {
+      max-width: 1280px;
+      margin: 0 auto;
+      padding-inline: 24px;
+    }
   }
 `
 
 export default GlobalStyle
+
