b2c651b800a2565f94fbf3bcf6c24c523ecc8aef
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-08-02 09:09:22 +0200

    detailed styling of buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const ButtonGroup = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 8px;
-  //flex-wrap: nowrap;
+  flex-wrap: nowrap;
   margin-top: 12px;
 
   @media (min-width: 668px) {
@@ -19,16 +20,21 @@ export const PrimaryButton = styled.a`
   color: var(--color-text);
   background-color: var(--color-primary);
   border: none;
-  width: fit-content;
+  width: 100%;
+  text-align: center;
 
   @media (min-width: 668px) {
     font-size: 14px;
-    padding: 6px 8px;
+    padding: 6px 6px;
+    width: 130px;
+    height: 75px;
   }
 
   @media (min-width: 1024px) {
-    padding: 8px 10px;
+    padding: 8px 8px;
     font-size: 16px;
+    width: 230px;
+    height: 60px;
   }
 `;
 
@@ -38,15 +44,22 @@ export const SecondaryButton = styled.a`
   color: var(--color-text);
   text-decoration: none;
   border-radius: 4px;
-  width: fit-content;
+  width: 100%;
+  text-align: center;
 
   @media (min-width: 668px) {
-    padding: 6px 8px;
+    padding: 6px 6px;
     font-size: 14px;
+    width: 130px;
+    height: 75px;
+    margin: 0 auto;
   }
 
   @media (min-width: 1024px) {
-    padding: 8px 10px;
+    padding: 8px 8px;
     font-size: 16px;
+    width: 230px;
+    height: 60px;
+    margin: 0 auto;
   }
 `;
