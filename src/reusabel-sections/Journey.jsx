8cef6bcf5f16d1b04ffe05b600299a01eca960dd
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-10-05 17:04:54 +0200

    updated intro

--- src/reusabel-sections/Journey.jsx
+++ src/reusabel-sections/Journey.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import styled from "styled-components"
 
 import Card from "../components/Card.jsx"
 import DesktopCarousel from "../components/DesktopCarousel.jsx"
--- src/reusabel-sections/Projects.jsx
+++ src/reusabel-sections/Projects.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import styled from "styled-components"
 
 import Card from "../components/Card"
 import DesktopCarousel from "../components/DesktopCarousel"
